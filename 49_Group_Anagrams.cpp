1e3f32842f0d26c01ca9df3346ba30289107ad82
Prashant <prashantagarwal695@gmail.com>
2021-08-12 15:10:11 +0530

    commit

--- 49_Group_Anagrams.cpp
+++ 49_Group_Anagrams.cpp
@@ -1,3 +1,30 @@
+// Method_1
+// map<string,vector<string>>m;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+       map<string,vector<string>>m;
+        vector<vector<string>>vv;
+        string x;
+        for(int i=0;i<strs.size();i++)
+        {
+          x=strs[i];
+          sort(x.begin(),x.end()); 
+          m[x].push_back(strs[i]);  
+        }
+      
+        for(auto it:m)
+        {
+            vv.push_back(it.second);
+        }
+        return vv;
+    }
+};
+
+// Method_1
+// vector<pair<string, int>> v;
+
 
 class Solution
 {
