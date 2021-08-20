f732f0f2a2e223e88452db01c8deb35b00d39f78
Prashant <prashantagarwal695@gmail.com>
2021-08-20 21:02:47 +0530

    commit

--- 763_Partition_Labels.cpp
+++ 763_Partition_Labels.cpp
@@ -1,3 +1,24 @@
+// Method_1
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int last[26]={0};
+        vector<int>res;
+        for(int i=0;s[i];i++) last[s[i]-'a']=i;
+        int start=0,end=0;
+        for(int i=0;s[i];i++)
+        {
+            end=max(end,last[s[i]-'a']);
+            if(i==end){
+                res.push_back(i-start+1);
+                start=i+1;
+            }
+        }
+        return res;
+    }
+};
+
+// Method_2
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& v) {
