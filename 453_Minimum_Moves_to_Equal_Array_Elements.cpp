9cc01cfc73050d10d82ab462ae54762f83c6e877
Prashant <prashantagarwal695@gmail.com>
2021-08-21 18:10:16 +0530

    commit

---
+++ 453_Minimum_Moves_to_Equal_Array_Elements.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minMoves(vector<int>& nums) {
+        int minn=INT_MAX,res=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(minn>nums[i]) minn=nums[i];
+        }
+        for(int i=0;i<nums.size();i++)
+            res+=(nums[i]-minn);
+        return res;
+    }
+};
