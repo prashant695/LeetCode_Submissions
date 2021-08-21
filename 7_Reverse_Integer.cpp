6242f0943b36633cf11a016c11f93060e7fdf5a7
Prashant <prashantagarwal695@gmail.com>
2021-08-21 17:55:41 +0530

    commit

---
+++ 7_Reverse_Integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) 
+    {
+        long rev=0,flg=0;
+        if(x<0) flg=1;
+        x=abs(x);
+        while(x)
+        {
+           rev=(x%10)+(rev*10);
+           if(rev>=INT_MAX) return 0;
+           if(flg && (rev*-1)<=INT_MIN) return 0;
+            x/=10;
+        }
+        if(flg) rev*=-1;
+        return rev;
+    }
+};
