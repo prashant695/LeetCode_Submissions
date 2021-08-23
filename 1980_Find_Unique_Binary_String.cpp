da0d705234b053cbc1686dc304104362602b66a6
Prashant <prashantagarwal695@gmail.com>
2021-08-23 15:30:11 +0530

    commit

---
+++ 1980_Find_Unique_Binary_String.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        map<string,int>m;
+        int n=nums.size();
+        string s(n,'0');
+        int j=nums.size()-1;
+        for(int i=0;i<=nums.size();i++) m[nums[i]]++;
+        for(int i=0;i<=n;i++)
+        {
+            if(i==0) if(m[s]==0) return s; 
+            else s[j--]='1';
+            cout<<s<<endl;
+            if(m[s]==0) return s;
+        }
+        return "";
+    }
+};
