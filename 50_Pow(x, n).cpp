90c6c95ba1b9bcf8ebc0882fbe7663f497dde007
Prashant <prashantagarwal695@gmail.com>
2021-09-04 22:55:36 +0530

    commit

---
+++ 50_Pow(x, n).cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n==0) return 1;
+        else if(n==1) return x;
+        else if(n==2) return x*x;
+        else if(n<0) return 1/x * myPow(1/x, -(n+1));
+        else 
+        {
+            if(n%2==1) return x*(myPow(x*x,n/2));
+            else return (myPow(x*x,n/2));
+        }
+    }    
+};
