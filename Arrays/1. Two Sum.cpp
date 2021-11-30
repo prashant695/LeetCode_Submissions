64e514653826a3690cb76b22c15b4de20fb76d5d
Prashant <prashantagarwal695@gmail.com>
2021-11-30 15:14:55 +0530

    updated two sum.cpp

---
+++ Arrays/1. Two Sum.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> nums, int target)
+    {
+        map<int, int> m;
+        for (int i = 0; i < nums.size(); i++)
+            m[nums[i]] = i;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (m[target - nums[i]])
+            {
+                if (i != m[target - nums[i]])
+                {
+                    return {i, m[target - nums[i]]};
+                }
+            }
+        }
+        return {};
+    }
+};
