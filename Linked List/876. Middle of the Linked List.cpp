a7468ffc9c3f08200a37a55b218a52b93bafbd1d
Prashant <prashantagarwal695@gmail.com>
2021-12-28 12:04:42 +0530

    added more approaches

--- Linked List/876. Middle of the Linked List.cpp
+++ Linked List/876. Middle of the Linked List.cpp
@@ -1,3 +1,6 @@
+// Method-1
+// storing length of linked list
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -13,6 +16,33 @@ class Solution
 public:
     ListNode *middleNode(ListNode *head)
     {
+        ListNode *tmp = head;
+        int node = 0, cnt = 0;
+        while (tmp)
+        {
+            node++;
+            tmp = tmp->next;
+        }
+        tmp = head;
+        while (tmp)
+        {
+            cnt++;
+            if (cnt == ceil((node + 1) / 2.0))
+                return tmp;
+            tmp = tmp->next;
+        }
+        return NULL;
+    }
+};
+
+// Method-2
+// Concept of even and odd
+
+class Solution
+{
+public:
+    ListNode *middleNode(ListNode *head)
+    {
         ListNode *mid = head;
         int cnt = 0;
         while (head)
@@ -23,5 +53,22 @@ public:
             head = head->next;
         }
         return mid;
+    }
+};
+
+// Method-3
+// Hare and Turtle
+class Solution
+{
+public:
+    ListNode *middleNode(ListNode *head)
+    {
+        ListNode *turtle = head, *hare = head;
+        while (hare and hare->next)
+        {
+            turtle = turtle->next;
+            hare = hare->next->next;
+        }
+        return turtle;
     }
 };
