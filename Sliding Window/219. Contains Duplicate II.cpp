e6c0fd6bc079d5ca1cc8fac389d7e7810d7ce3a3
Prashant <prashantagarwal695@gmail.com>
2022-01-03 14:45:34 +0530

    update sliding window

---
+++ Sliding Window/219. Contains Duplicate II.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    bool containsNearbyDuplicate(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (m[nums[i]] == 0)
+                m[nums[i]] = i + 1;
+            else if (i + 1 - m[nums[i]] <= k)
+                return 1;
+            else
+                m[nums[i]] = i + 1;
+        }
+        return 0;
+    }
+};
